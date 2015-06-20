801595e3c8569a507a5faac75e1dfa6bc8ca49f2
NicolasBezerra <nicolasbezerra14@gmail.com>
2015-06-20 14:31:16 -0300

    Calculadora de Binário (completa)
    -Converte para decimal e hexadecimal
    -Soma, subtrai, multiplica e divide binário

---
+++ CalculadoraDeBinário/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace CalculadoraDeBinário
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDecimal = new System.Windows.Forms.TextBox();
+            this.textBoxHexadecimal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxBinary2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxBinary = new System.Windows.Forms.TextBox();
+            this.BinaryLabel = new System.Windows.Forms.Label();
+            this.btDiv = new System.Windows.Forms.Button();
+            this.btMult = new System.Windows.Forms.Button();
+            this.btSub = new System.Windows.Forms.Button();
+            this.btSum = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxOperation = new System.Windows.Forms.TextBox();
+            this.btConv = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(280, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Decimal:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(280, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "hexadecimal:";
+            // 
+            // textBoxDecimal
+            // 
+            this.textBoxDecimal.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxDecimal.Location = new System.Drawing.Point(362, 16);
+            this.textBoxDecimal.Name = "textBoxDecimal";
+            this.textBoxDecimal.Size = new System.Drawing.Size(133, 26);
+            this.textBoxDecimal.TabIndex = 24;
+            // 
+            // textBoxHexadecimal
+            // 
+            this.textBoxHexadecimal.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxHexadecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxHexadecimal.Location = new System.Drawing.Point(393, 61);
+            this.textBoxHexadecimal.Name = "textBoxHexadecimal";
+            this.textBoxHexadecimal.Size = new System.Drawing.Size(102, 26);
+            this.textBoxHexadecimal.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label5.Location = new System.Drawing.Point(12, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 20);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Valor Binário ² :";
+            // 
+            // textBoxBinary2
+            // 
+            this.textBoxBinary2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxBinary2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxBinary2.Location = new System.Drawing.Point(145, 58);
+            this.textBoxBinary2.Name = "textBoxBinary2";
+            this.textBoxBinary2.Size = new System.Drawing.Size(113, 26);
+            this.textBoxBinary2.TabIndex = 35;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(229, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Resultado:";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxResult.ForeColor = System.Drawing.Color.DarkRed;
+            this.textBoxResult.Location = new System.Drawing.Point(213, 150);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(132, 26);
+            this.textBoxResult.TabIndex = 33;
+            // 
+            // textBoxBinary
+            // 
+            this.textBoxBinary.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxBinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxBinary.Location = new System.Drawing.Point(145, 16);
+            this.textBoxBinary.Name = "textBoxBinary";
+            this.textBoxBinary.Size = new System.Drawing.Size(113, 26);
+            this.textBoxBinary.TabIndex = 32;
+            // 
+            // BinaryLabel
+            // 
+            this.BinaryLabel.AutoSize = true;
+            this.BinaryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.BinaryLabel.Location = new System.Drawing.Point(12, 19);
+            this.BinaryLabel.Name = "BinaryLabel";
+            this.BinaryLabel.Size = new System.Drawing.Size(127, 20);
+            this.BinaryLabel.TabIndex = 31;
+            this.BinaryLabel.Text = "Valor Binário ¹ :";
+            // 
+            // btDiv
+            // 
+            this.btDiv.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btDiv.Location = new System.Drawing.Point(16, 184);
+            this.btDiv.Name = "btDiv";
+            this.btDiv.Size = new System.Drawing.Size(50, 38);
+            this.btDiv.TabIndex = 42;
+            this.btDiv.Text = "÷";
+            this.btDiv.UseVisualStyleBackColor = true;
+            this.btDiv.Click += new System.EventHandler(this.btDiv_Click);
+            // 
+            // btMult
+            // 
+            this.btMult.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btMult.Location = new System.Drawing.Point(72, 184);
+            this.btMult.Name = "btMult";
+            this.btMult.Size = new System.Drawing.Size(53, 38);
+            this.btMult.TabIndex = 41;
+            this.btMult.Text = "x";
+            this.btMult.UseVisualStyleBackColor = true;
+            this.btMult.Click += new System.EventHandler(this.btMult_Click);
+            // 
+            // btSub
+            // 
+            this.btSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btSub.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btSub.Location = new System.Drawing.Point(72, 136);
+            this.btSub.Name = "btSub";
+            this.btSub.Size = new System.Drawing.Size(53, 40);
+            this.btSub.TabIndex = 40;
+            this.btSub.Text = "-";
+            this.btSub.UseVisualStyleBackColor = true;
+            this.btSub.Click += new System.EventHandler(this.btSub_Click);
+            // 
+            // btSum
+            // 
+            this.btSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btSum.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btSum.Location = new System.Drawing.Point(16, 136);
+            this.btSum.Name = "btSum";
+            this.btSum.Size = new System.Drawing.Size(50, 42);
+            this.btSum.TabIndex = 39;
+            this.btSum.Text = "+";
+            this.btSum.UseVisualStyleBackColor = true;
+            // 
+            // bt0
+            // 
+            this.bt0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.bt0.Location = new System.Drawing.Point(131, 182);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(50, 40);
+            this.bt0.TabIndex = 38;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.bt1.Location = new System.Drawing.Point(131, 136);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(50, 40);
+            this.bt1.TabIndex = 37;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 18);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Operação:";
+            // 
+            // textBoxOperation
+            // 
+            this.textBoxOperation.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBoxOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBoxOperation.Location = new System.Drawing.Point(95, 99);
+            this.textBoxOperation.Name = "textBoxOperation";
+            this.textBoxOperation.Size = new System.Drawing.Size(27, 26);
+            this.textBoxOperation.TabIndex = 43;
+            // 
+            // btConv
+            // 
+            this.btConv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btConv.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.btConv.Location = new System.Drawing.Point(393, 151);
+            this.btConv.Name = "btConv";
+            this.btConv.Size = new System.Drawing.Size(102, 31);
+            this.btConv.TabIndex = 45;
+            this.btConv.Text = "converter";
+            this.btConv.UseVisualStyleBackColor = true;
+            this.btConv.Click += new System.EventHandler(this.btConv_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btClear.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.btClear.Location = new System.Drawing.Point(393, 111);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(102, 34);
+            this.btClear.TabIndex = 47;
+            this.btClear.Text = "Limpar";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btResult
+            // 
+            this.btResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btResult.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.btResult.Location = new System.Drawing.Point(393, 188);
+            this.btResult.Name = "btResult";
+            this.btResult.Size = new System.Drawing.Size(102, 34);
+            this.btResult.TabIndex = 46;
+            this.btResult.Text = "Resultado";
+            this.btResult.UseVisualStyleBackColor = true;
+            this.btResult.Click += new System.EventHandler(this.btResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 334);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btResult);
+            this.Controls.Add(this.btConv);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxOperation);
+            this.Controls.Add(this.btDiv);
+            this.Controls.Add(this.btMult);
+            this.Controls.Add(this.btSub);
+            this.Controls.Add(this.btSum);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxBinary2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxBinary);
+            this.Controls.Add(this.BinaryLabel);
+            this.Controls.Add(this.textBoxHexadecimal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDecimal);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Calculadora de Binário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDecimal;
+        private System.Windows.Forms.TextBox textBoxHexadecimal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxBinary2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxBinary;
+        private System.Windows.Forms.Label BinaryLabel;
+        private System.Windows.Forms.Button btDiv;
+        private System.Windows.Forms.Button btMult;
+        private System.Windows.Forms.Button btSub;
+        private System.Windows.Forms.Button btSum;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxOperation;
+        private System.Windows.Forms.Button btConv;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btResult;
+
+    }
+}
+
---
+++ CalculadoraDeBinário/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDeBinário
+{
+    public partial class Form1 : Form
+    {
+        public string NumberToOperator;
+        public int binaryNum;
+        public int decimalNum;
+        public string HexaNum;
+
+        public Form1()
+        { InitializeComponent(); }
+
+        public static string InverterString(string str)
+        {
+
+            int tamanho = str.Length; char[] caracteres = new char[tamanho];
+            for (int i = 0; i < tamanho; i++)
+            { caracteres[i] = str[tamanho - 1 - i]; } return new string(caracteres);
+        }
+
+        public static string BinarySub(string a, string b)
+        {
+
+            string n1 = stringToNormalize(a, b)[0]; string n2 = stringToNormalize(a, b)[1];
+            n2 = BinarySum(BinaryNumberBubstitution(n2), "1"); string result = BinarySum(n1, n2);
+            result = result.Remove(0, 1); return result;
+        }
+
+        public static string[] BinaryDiv(string a, string b)
+        {
+
+            string In = "";
+
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                In = BinaryMult(b, decimalToBinary(i));
+                if (BinaryIsBetter(In, a)) { return new string[2] { decimalToBinary(i - 1), BinarySub(a, BinarySub(In, b)) }; }
+                else if (In == a) { return new string[2] { decimalToBinary(i), "0" }; }
+            } return new string[2];
+        }
+
+        public static string decimalToBinary(double b)
+        {
+
+            double rest = b % 2; double result = (b - rest) / 2;
+            if (b == 1) { return rest.ToString(); } else { return decimalToBinary(result) + rest.ToString(); }
+        }
+
+        public static int BinaryToDecimal(string valorBinario)
+        {
+
+            int exp = 0;
+            int n;
+            int sum = 0;
+            string numeroInvertido = InverterString(valorBinario);
+
+            for (int i = 0; i < numeroInvertido.Length; i++)
+            { n = Convert.ToInt32(numeroInvertido.Substring(i, 1)); sum += n * (int)Math.Pow(2, exp); exp++; } return sum;
+        }
+
+        public static int BinaryToDecimal2(string valorBinario)
+        { return Convert.ToInt32(valorBinario, 2); }
+
+        public static bool BinaryIsBetter(string e1, string e2)
+        {
+
+            if (BinaryToDecimal(e1) > BinaryToDecimal(e2)) return true;
+            else if (BinaryToDecimal(e2) > BinaryToDecimal(e1)) return false;
+            else { return false; }
+
+        }
+
+        static string Stringreverse(string Word)
+        {
+
+            char[] arrChar = Word.ToCharArray();
+            Array.Reverse(arrChar);
+            string reverse = new String(arrChar);
+
+            return reverse;
+        }
+
+        public static string[] stringToNormalize(string a, string b)
+        {
+
+            int numberWrong1 = a.Length; int numberWrong2 = b.Length;
+
+            if (a.Length > b.Length)
+                for (int i = 0; i < (numberWrong1 - numberWrong2); i++)
+                { b = "0" + b; }
+
+            else if (a.Length < b.Length)
+                for (int i = 0; i < (numberWrong2 - numberWrong1); i++)
+                { a = "0" + a; }
+
+            string[] r = new string[] { a, b }; return r;
+        }
+
+        public static string BinarySum(string a, string b)
+        {
+
+            char[] h = (stringToNormalize(a, b))[0].ToCharArray();
+            char[] s = (stringToNormalize(a, b))[1].ToCharArray();
+            string overflow = "";
+            string result = "";
+
+            for (int i = h.Length - 1; i >= 0; i--)
+            {
+                if (string.IsNullOrWhiteSpace(overflow))
+                {
+                    if (h[i].Equals('0') && s[i].Equals('0')) { result = '0' + result; }
+
+                    else if (h[i].Equals('0') && s[i].Equals('1')) { result = '1' + result; }
+                    else if (h[i].Equals('1') && s[i].Equals('0')) { result = '1' + result; }
+                    else if (h[i].Equals('1') && s[i].Equals('1')) { overflow = "1"; result = "0" + result; }
+                }
+
+                else if ((h[i].Equals('1') && s[i].Equals('0')) || h[i].Equals('0') && s[i].Equals('1'))
+                { overflow = "1"; result = "0" + result; }
+
+                else if ((h[i].Equals('0') && s[i].Equals('0'))) { overflow = ""; result = "1" + result; }
+
+                else if (h[i].Equals('1') && s[i].Equals('1')) { overflow = "1"; result = "1" + result; }
+            }
+
+            return overflow + result;
+        }
+
+        public static string BinaryMult(string a, string b)
+        {
+
+            string result = b;
+            for (int i = 0; i < BinaryToDecimal(a) - 1; i++) { result = BinarySum(b, result); } return result;
+        }
+
+        public static string BinaryNumberBubstitution(string str)
+        { string result = str; result = result.Replace('1', 'b'); result = result.Replace('0', '1'); result = result.Replace('b', '0'); return result; }
+
+        private void Clear() { this.textBoxBinary = null; }
+
+        private void bt1_Click(object sender, EventArgs e) { this.textBoxBinary.Text += bt1.Text; }
+
+        private void bt0_Click(object sender, EventArgs e) { this.textBoxBinary.Text += bt0.Text; }
+
+        private void btSum_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btSub_Click(object sender, EventArgs e)
+        { this.textBoxOperation.Text = "  -"; if (!textBoxBinary.Text.Equals("")) { NumberToOperator = textBoxDecimal.Text; } }
+
+        private void btMult_Click(object sender, EventArgs e)
+        { this.textBoxOperation.Text = "  x"; if (!textBoxBinary.Text.Equals("")) { NumberToOperator = textBoxDecimal.Text; } }
+
+        private void btDiv_Click(object sender, EventArgs e)
+        { this.textBoxOperation.Text = "  ÷"; if (!textBoxBinary.Text.Equals("")) { NumberToOperator = textBoxDecimal.Text; } }
+
+        private static string HexaNumbers(string n)
+        {
+
+            switch (n)
+            {
+
+                case "10": return "A"; break;
+                case "11": return "B"; break;
+                case "12": return "C"; break;
+                case "13": return "D"; break;
+                case "14": return "E"; break;
+                case "15": return "F"; break;
+                default: return n;
+            }
+        }
+
+        public static string DecimalToHexadecimal(double n)
+        {
+            double rest = n % 16; double result = (n - rest) / 16; if (n.Equals(0)) { return ""; }
+            else { return DecimalToHexadecimal(result) + HexaNumbers(rest.ToString()); }
+        }
+
+        private void btConv_Click(object sender, EventArgs e)
+        { textBoxDecimal.Text = BinaryToDecimal(textBoxBinary.Text).ToString(); textBoxHexadecimal.Text = DecimalToHexadecimal(Convert.ToDouble(textBoxDecimal.Text)); }
+
+        private void btResult_Click(object sender, EventArgs e)
+        {
+            if (this.textBoxOperation.Text.Equals("  +")) { textBoxResult.Text = BinarySum(textBoxBinary.Text, textBoxBinary2.Text); }
+            else if (this.textBoxOperation.Text.Equals("  x")) { textBoxResult.Text = BinaryMult(textBoxBinary.Text, textBoxBinary2.Text); }
+            else if (this.textBoxOperation.Text.Equals("  ÷")) { string[] n = BinaryDiv(textBoxBinary.Text, textBoxBinary2.Text); textBoxResult.Text = n[0] + " Re:" + n[1]; }
+            else if (this.textBoxOperation.Text.Equals("  -")) { textBoxResult.Text = BinarySub(textBoxBinary.Text, textBoxBinary2.Text); }
+        }
+
+
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            textBoxBinary2.Text = null; textBoxDecimal.Text = null; textBoxBinary.Text = null;
+            textBoxHexadecimal.Text = null; textBoxOperation.Text = null; textBoxResult.Text = null;
+        }
+
+        private void KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar) && !(e.KeyChar == (char)Keys.Space)) { e.Handled = true; }
+            string caracteresPermitidos = "01";
+            if (!(caracteresPermitidos.Contains(e.KeyChar.ToString().ToUpper()))) { e.Handled = true; }
+        }
+
+        private void NoPressed(object sender, KeyPressEventArgs e)
+        { string caracteresPermitidos = ""; if (!(caracteresPermitidos.Contains(e.KeyChar.ToString().ToUpper()))) { e.Handled = true; } }
+
+
+
+    }
+}
+
+
